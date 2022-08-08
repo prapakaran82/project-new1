85d74dd2650b07bdd1b9d26214e0fc034771ba68
prapakaran82 <sugunaselvi1991@gmail.com>
2022-08-08 08:45:55 +0530

    validate prime and test

--- src/test/java/org/gitt1/PrimeNumber.java
+++ src/test/java/org/gitt1/PrimeNumber.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 public class PrimeNumber {
 	@Test
 	private void test2() {
+		String s="prapakaran";
 System.out.println("hello world");
+System.out.println(s);
 	}
 
 }
--- src/test/java/org/gitt1/Testting.java
+++ src/test/java/org/gitt1/Testting.java
@@ -1,5 +1,11 @@
 package org.gitt1;
 
+import org.junit.Test;
+
 public class Testting {
+	@Test
+	private void test1() {
+System.out.println("test1");
+	}
 
 }
